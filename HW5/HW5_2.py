f4645944194afe6d7184e36bc0402119d7d95ffe
mlchung1231 <L35104132@gmail.com>
2024-10-17 23:26:04 +0800

    HW5

--- HW5/HW5_2.py
+++ HW5/HW5_2.py
@@ -1,154 +1,76 @@
 import numpy as np
-from tensorflow.keras.datasets import mnist
+import pandas as pd
 import matplotlib.pyplot as plt
-import cv2
-from skimage import morphology
-import random
 
-
-class myNaiveBayesClassifier:
-
-    def fit(self, X, y):
-        self.classes, self.y_counts = np.unique(y, return_counts=True)
-        self.P_y = self.y_counts / y.shape[0]
-        self.c_x_y = np.zeros((len(self.classes), X.shape[1]));
-        self.P_x_y = np.zeros((2 * len(self.classes), X.shape[1]));
-        for n in range(len(y)):
-            self.c_x_y[y[n]] += X[n]
-        self.c_x_y += 1 
-        for c in range(len(self.classes)):
-            self.P_x_y[2*c + 1] = self.c_x_y[c] / (self.y_counts[c] + 1)
-            self.P_x_y[2*c] = 1 - self.P_x_y[2*c + 1]
-            
-        self.save_samples(X, y)
-            
-        
+class myNB_Gaussian_Classifier:
+    
+    def fit(self, df):
+        self.male_stats = {'mean': df[df['Person'] == 1][['height', 'weight', 'foot size']].mean(),
+                           'var': df[df['Person'] == 1][['height', 'weight', 'foot size']].var()}
+        self.female_stats = {'mean': df[df['Person'] == 0][['height', 'weight', 'foot size']].mean(),
+                             'var': df[df['Person'] == 0][['height', 'weight', 'foot size']].var()}
+    
     def predict(self, X):
-        predictions = []
-        probability = []
-        for n in range(X.shape[0]):
-            P_y_x = []
-            for c in range(len(self.classes)):
-                temp_1 = X[n] * self.P_x_y[2 * c + 1]
-                temp_0 = (1 - X[n]) * self.P_x_y[2 * c] 
-                temp = np.log(temp_0 + temp_1)
-                P_xs_y_log = np.sum(temp)
-                P_xs_y = np.exp(P_xs_y_log)
-                P_y_x.append(P_xs_y * self.P_y[c])
-            max_class = np.argmax(P_y_x)
-            P_x = np.sum(P_y_x)
-            predictions.append(max_class)
-            probability.append(P_y_x[max_class] / P_x)
-        return np.array(predictions), np.array(probability)
-    
-    def plot_P_x_y(self):
-        fig, axes = plt.subplots(2, 5, figsize=(12, 5))
-        axes = axes.ravel()
-        for i in range(10):
-            img_P_x_y = self.P_x_y[2 *i + 1].reshape(28,28)
-            axes[i].imshow(img_P_x_y, cmap='gray')
-            axes[i].set_title(f"Label: {i}", fontsize = 12)
-            axes[i].axis('off')
-        plt.tight_layout()
-        plt.show()
-    
-    def save_samples(self, X, y):
-        self.Xs_dict = {i: [] for i in range(10)}
-        for i in range(len(y)):
-            self.Xs_dict[y[i]].append(X[i])
-        for key in self.Xs_dict:
-            self.Xs_dict[key] = np.array(self.Xs_dict[key])
+        P_male = 0.5
+        P_female = 0.5
+        for i, feature in enumerate(sample):
+            P_male *= self.gaussian_distribution(feature, self.male_stats['mean'][i], self.male_stats['var'][i])
+        for i, feature in enumerate(sample):
+            P_female *= self.gaussian_distribution(feature, self.female_stats['mean'][i], self.female_stats['var'][i])
             
-    def decide_class(self, Y):
-        if isinstance(Y, int):
-            return Y
-        else:
-            Y_values = list(range(10))
-            random_number = np.random.choice(Y_values, size=1, p=Y)
-            gen_Y = random_number.item()
-            return gen_Y
+        if P_male >= P_female:
+            return 'male'
+        else: 
+            return 'female'
             
+    def gaussian_distribution(self, x, mean, variance):
+        exponent = np.exp(-((x - mean) ** 2) / (2 * variance))
+        return (1 / np.sqrt(2 * np.pi * variance)) * exponent
     
-    def generate_img(self, Y):
-        Y = self.decide_class(Y)
-        random_matrix = np.random.rand(28, 28)
-        x_prob = self.P_x_y[2 * Y + 1].reshape(28, 28)
-        img = x_prob * random_matrix 
-        out_img = self.improve_img(img, 0.18)
-        return out_img, Y
-    
-    def generate_img2(self, Y):
-        Y = self.decide_class(Y)
-        random_matrix1 = np.random.rand(28, 28)
-        random_matrix2 = np.random.rand(28, 28)
-        sample_i = random.randint(0, self.Xs_dict[Y].shape[0]-1)
-        sample_x = (self.Xs_dict[Y][sample_i].reshape(28,28) + 0.01) * random_matrix1
-        x_prob = self.P_x_y[2 * Y + 1].reshape(28, 28)
-        img = x_prob * random_matrix2 * sample_x
-        out_img = self.improve_img(img, 0.03)
-        return img, out_img, Y
-    
-    def improve_img(self, img, threshold):
-        blur_img = cv2.GaussianBlur(img, (3, 3), 0)
-        img_binary = np.where(blur_img > threshold, 1, 0)
-        skeleton = morphology.skeletonize(img_binary).astype(np.float32)
-        blur_img_2 = cv2.blur(skeleton, (2, 2))
-        img_bright = blur_img_2 * 2
-        img_bright = np.where(img_bright > 1, 1, img_bright)
-        return img_bright
+    def plot_distributions(self):
+        features = ['height', 'weight', 'foot size']
+        for i, feature in enumerate(features):
 
+            x_min = min(self.male_stats['mean'][i] - 4 * np.sqrt(self.male_stats['var'][i]),
+                         self.female_stats['mean'][i] - 4 * np.sqrt(self.female_stats['var'][i]))
+            x_max = max(self.male_stats['mean'][i] + 4 * np.sqrt(self.male_stats['var'][i]),
+                         self.female_stats['mean'][i] + 4 * np.sqrt(self.female_stats['var'][i]))
+            x = np.linspace(x_min, x_max, 100)
+            
+            # Male and Female PDFs
+            male_distribution = self.gaussian_distribution(x, self.male_stats['mean'][i], self.male_stats['var'][i])
+            female_distribution = self.gaussian_distribution(x, self.female_stats['mean'][i], self.female_stats['var'][i])
+            
+            plt.figure(figsize=(10, 5))
+            plt.plot(x, male_distribution, label='Male Distribution', color='blue')
+            plt.plot(x, female_distribution, label='Female Distribution', color='red')
+            plt.title(f'Probability Distribution of {feature}', fontsize = 20)
+            plt.xlabel(feature, fontsize = 16)
+            plt.ylabel('Probability Density', fontsize = 16)
+            plt.legend()
+            plt.grid()
+            plt.show()
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# 示例训练数据
+data = {
+    'height': [6, 5.92, 5.58, 5.92, 5, 5.5, 5.42, 5.75],
+    'weight': [180, 190, 170, 165, 100, 150, 130, 150],
+    'foot size': [12, 11, 12, 10, 6, 8, 7, 9],
+    'Person': ['male', 'male', 'male', 'male', 'female', 'female', 'female', 'female']
+}
 
-x_train_flat = x_train.reshape(-1, 28 * 28) / 255.0
-x_test_flat = x_test.reshape(-1, 28 * 28) / 255.0
+# 创建DataFrame
+df_train = pd.DataFrame(data)
 
-threshold = 0.2
-x_train_binary = (x_train_flat > threshold).astype(np.float32)
-x_test_binary = (x_test_flat > threshold).astype(np.float32)
+# 将性别转换为数值
+df_train['Person'] = df_train['Person'].map({'male': 1, 'female': 0})
 
+model = myNB_Gaussian_Classifier()
+model.fit(df_train)
 
+model.plot_distributions()
 
-nb_classifier = myNaiveBayesClassifier()
-nb_classifier.fit(x_train_binary, y_train)
-nb_classifier.plot_P_x_y()
-
-Y1 = 3
-Y2 = [0.05, 0.05, 0.05, 0.55, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05]
-
-fig, axes = plt.subplots(2, 5, figsize=(12, 5))
-axes = axes.ravel()
-for i in range(10):
-    ori, gen_img, gen_Y = nb_classifier.generate_img2(Y2)
-    axes[i].imshow(ori, cmap='gray')
-    axes[i].set_title(f"Label: {gen_Y}", fontsize = 12)
-    axes[i].axis('off')
-plt.tight_layout()
-plt.show()
-
-fig, axes = plt.subplots(2, 5, figsize=(12, 5))
-axes = axes.ravel()
-for i in range(10):
-    ori, gen_img, gen_Y = nb_classifier.generate_img2(Y2)
-    axes[i].imshow(gen_img, cmap='gray')
-    axes[i].set_title(f"Label: {gen_Y}", fontsize = 12)
-    axes[i].axis('off')
-plt.tight_layout()
-plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 示例分类
+sample = (6, 130, 8)  # 待分类样本（身高，体重，鞋码）
+prediction = model.predict(sample)
+print(f'{sample[0]} feet, {sample[1]} lbs, {sample[2]} inches is: {prediction}')
---
+++ HW5/HW5_3.py
@@ -0,0 +1,142 @@
+import numpy as np
+from tensorflow.keras.datasets import mnist
+import matplotlib.pyplot as plt
+import cv2
+from skimage import morphology
+import random
+
+
+class myNaiveBayesClassifier:
+
+    def fit(self, X, y):
+        self.classes, self.y_counts = np.unique(y, return_counts=True)
+        self.P_y = self.y_counts / y.shape[0]
+        self.c_x_y = np.zeros((len(self.classes), X.shape[1]));
+        self.P_x_y = np.zeros((2 * len(self.classes), X.shape[1]));
+        for n in range(len(y)):
+            self.c_x_y[y[n]] += X[n]
+        self.c_x_y += 1 
+        for c in range(len(self.classes)):
+            self.P_x_y[2*c + 1] = self.c_x_y[c] / (self.y_counts[c] + 1)
+            self.P_x_y[2*c] = 1 - self.P_x_y[2*c + 1]
+            
+        self.save_samples(X, y)
+            
+        
+    def predict(self, X):
+        predictions = []
+        probability = []
+        for n in range(X.shape[0]):
+            P_y_x = []
+            for c in range(len(self.classes)):
+                temp_1 = X[n] * self.P_x_y[2 * c + 1]
+                temp_0 = (1 - X[n]) * self.P_x_y[2 * c] 
+                temp = np.log(temp_0 + temp_1)
+                P_xs_y_log = np.sum(temp)
+                P_xs_y = np.exp(P_xs_y_log)
+                P_y_x.append(P_xs_y * self.P_y[c])
+            max_class = np.argmax(P_y_x)
+            P_x = np.sum(P_y_x)
+            predictions.append(max_class)
+            probability.append(P_y_x[max_class] / P_x)
+        return np.array(predictions), np.array(probability)
+    
+    def plot_P_x_y(self):
+        fig, axes = plt.subplots(2, 5, figsize=(12, 5))
+        axes = axes.ravel()
+        for i in range(10):
+            img_P_x_y = self.P_x_y[2 *i + 1].reshape(28,28)
+            axes[i].imshow(img_P_x_y, cmap='gray')
+            axes[i].set_title(f"Label: {i}", fontsize = 12)
+            axes[i].axis('off')
+        plt.tight_layout()
+        plt.show()
+    
+    def save_samples(self, X, y):
+        self.Xs_dict = {i: [] for i in range(10)}
+        for i in range(len(y)):
+            self.Xs_dict[y[i]].append(X[i])
+        for key in self.Xs_dict:
+            self.Xs_dict[key] = np.array(self.Xs_dict[key])
+            
+    def decide_class(self, Y):
+        if isinstance(Y, int):
+            return Y
+        else:
+            Y_values = list(range(10))
+            random_number = np.random.choice(Y_values, size=1, p=Y)
+            gen_Y = random_number.item()
+            return gen_Y
+            
+    
+    def generate_img(self, Y):
+        Y = self.decide_class(Y)
+        random_matrix = np.random.rand(28, 28)
+        x_prob = self.P_x_y[2 * Y + 1].reshape(28, 28)
+        img = x_prob * random_matrix 
+        out_img = self.improve_img(img, 0.18)
+        return out_img, Y
+    
+    def generate_img2(self, Y):
+        Y = self.decide_class(Y)
+        random_matrix1 = np.random.rand(28, 28)
+        random_matrix2 = np.random.rand(28, 28)
+        sample_i = random.randint(0, self.Xs_dict[Y].shape[0]-1)
+        sample_x = (self.Xs_dict[Y][sample_i].reshape(28,28) + 0.01) * random_matrix1
+        x_prob = self.P_x_y[2 * Y + 1].reshape(28, 28)
+        img = x_prob * random_matrix2 * sample_x
+        out_img = self.improve_img(img, 0.03)
+        return out_img, Y
+    
+    def improve_img(self, img, threshold):
+        blur_img = cv2.GaussianBlur(img, (3, 3), 0)
+        img_binary = np.where(blur_img > threshold, 1, 0)
+        skeleton = morphology.skeletonize(img_binary).astype(np.float32)
+        blur_img_2 = cv2.blur(skeleton, (2, 2))
+        img_bright = blur_img_2 * 2
+        img_bright = np.where(img_bright > 1, 1, img_bright)
+        return img_bright
+
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train_flat = x_train.reshape(-1, 28 * 28) / 255.0
+x_test_flat = x_test.reshape(-1, 28 * 28) / 255.0
+
+threshold = 0.2
+x_train_binary = (x_train_flat > threshold).astype(np.float32)
+x_test_binary = (x_test_flat > threshold).astype(np.float32)
+
+nb_classifier = myNaiveBayesClassifier()
+nb_classifier.fit(x_train_binary, y_train)
+
+Y1 = 3
+Y2 = [0.05, 0.05, 0.05, 0.55, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05]
+
+fig, axes = plt.subplots(2, 5, figsize=(12, 5))
+axes = axes.ravel()
+for i in range(10):
+    gen_img, gen_Y = nb_classifier.generate_img2(Y2)
+    axes[i].imshow(gen_img, cmap='gray')
+    axes[i].set_title(f"Label: {gen_Y}", fontsize = 12)
+    axes[i].axis('off')
+plt.tight_layout()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
